d9dbcbdcbeec405d778145193a4809ce293e6136
red-gezi <506065448@qq.com>
2022-10-27 18:06:11 +0800

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace NovelAIWindowsTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Text2Img = new System.Windows.Forms.Button();
+            this.btn_Text2Imgs = new System.Windows.Forms.Button();
+            this.btn_Config = new System.Windows.Forms.Button();
+            this.btn_images = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Text2Img
+            // 
+            this.btn_Text2Img.Location = new System.Drawing.Point(65, 13);
+            this.btn_Text2Img.Name = "btn_Text2Img";
+            this.btn_Text2Img.Size = new System.Drawing.Size(107, 50);
+            this.btn_Text2Img.TabIndex = 0;
+            this.btn_Text2Img.Text = "文生图";
+            this.btn_Text2Img.UseVisualStyleBackColor = true;
+            this.btn_Text2Img.Click += new System.EventHandler(this.btn_Text2Img_Click);
+            // 
+            // btn_Text2Imgs
+            // 
+            this.btn_Text2Imgs.Location = new System.Drawing.Point(65, 87);
+            this.btn_Text2Imgs.Name = "btn_Text2Imgs";
+            this.btn_Text2Imgs.Size = new System.Drawing.Size(107, 50);
+            this.btn_Text2Imgs.TabIndex = 1;
+            this.btn_Text2Imgs.Text = "批量文生图";
+            this.btn_Text2Imgs.UseVisualStyleBackColor = true;
+            this.btn_Text2Imgs.Click += new System.EventHandler(this.btn_Text2Imgs_Click);
+            // 
+            // btn_Config
+            // 
+            this.btn_Config.Location = new System.Drawing.Point(65, 157);
+            this.btn_Config.Name = "btn_Config";
+            this.btn_Config.Size = new System.Drawing.Size(107, 23);
+            this.btn_Config.TabIndex = 2;
+            this.btn_Config.Text = "打开配置文件";
+            this.btn_Config.UseVisualStyleBackColor = true;
+            this.btn_Config.Click += new System.EventHandler(this.btn_Config_Click);
+            // 
+            // btn_images
+            // 
+            this.btn_images.Location = new System.Drawing.Point(65, 186);
+            this.btn_images.Name = "btn_images";
+            this.btn_images.Size = new System.Drawing.Size(107, 23);
+            this.btn_images.TabIndex = 3;
+            this.btn_images.Text = "打开文生图文件夹";
+            this.btn_images.UseVisualStyleBackColor = true;
+            this.btn_images.Click += new System.EventHandler(this.btn_images_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(237, 221);
+            this.Controls.Add(this.btn_images);
+            this.Controls.Add(this.btn_Config);
+            this.Controls.Add(this.btn_Text2Imgs);
+            this.Controls.Add(this.btn_Text2Img);
+            this.Name = "Form1";
+            this.Text = "Ai画图桌面程序";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Text2Img;
+        private System.Windows.Forms.Button btn_Text2Imgs;
+        private System.Windows.Forms.Button btn_Config;
+        private System.Windows.Forms.Button btn_images;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NovelAIWindowsTools
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        string keyword => File.ReadAllLines("config.ini")[1];
+        int seed
+        {
+            get
+            {
+                var value = int.Parse(File.ReadAllLines("config.ini")[3]);
+                if (value == 0)
+                {
+                    value = random.Next(65535);
+                }
+                return value;
+            }
+        }
+        string model => int.Parse(File.ReadAllLines("config.ini")[5])==0? "safe-diffusion" : "nai-diffusion";
+
+        int generateCount => int.Parse(File.ReadAllLines("config.ini")[7]);
+        int width
+        {
+            get
+            {
+                var value = int.Parse(File.ReadAllLines("config.ini")[9]);
+                return (value / 64 + (value % 64 > 32 ? 1 : 0)) * 64;
+            }
+        }
+        int height
+        {
+            get
+            {
+                var value = int.Parse(File.ReadAllLines("config.ini")[11]);
+                return (value / 64 + (value % 64 > 32 ? 1 : 0)) * 64;
+            }
+        }
+        RestClient client;
+        RestRequest request;
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        public static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors) => true;
+
+        public void Init()
+        {
+            Directory.CreateDirectory("text2img");
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            client = new RestClient("https://api.novelai.net/ai/generate-image");
+            client.Timeout = -1;
+
+        }
+        public void TextToImage(int num)
+        {
+            request = new RestRequest(Method.POST);
+            request.AddHeader("authority", "api.novelai.net");
+            request.AddHeader("accept", "*/*");
+            request.AddHeader("accept-language", "zh-CN,zh;q=0.9");
+            request.AddHeader("authorization", "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjdJVTY1N3l4YzhVX3ZFVWU3dkwxSiIsIm5jIjoiY2NkSHoyc3BDc1luU29mWkloUUpzIiwiaWF0IjoxNjY2ODM5MjMwLCJleHAiOjE2Njk0MzEyMzB9.81py6Lr_qxgrAo8FTQXY_SBFPoRinLfdxK4X9xuispE");
+            request.AddHeader("content-type", "application/json");
+            request.AddHeader("origin", "https://novelai.net");
+            request.AddHeader("referer", "https://novelai.net/");
+            request.AddHeader("sec-ch-ua", "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"");
+            request.AddHeader("sec-ch-ua-mobile", "?0");
+            request.AddHeader("sec-ch-ua-platform", "\"Windows\"");
+            request.AddHeader("sec-fetch-dest", "empty");
+            request.AddHeader("sec-fetch-mode", "cors");
+            request.AddHeader("sec-fetch-site", "same-site");
+            client.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36";
+            var body = $@"{{""input"":""masterpiece, best quality,{keyword}"",""model"":""{model}"",""parameters"":{{""width"":{width},""height"":{height},""scale"":11,""sampler"":""k_euler_ancestral"",""steps"":28,""seed"":{seed + num},""n_samples"":1,""ucPreset"":0,""qualityToggle"":true,""uc"":""lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry""}}}}";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine("返回数据包长度"+response.Content.Length);
+            var data = response.Content.Replace("event: newImage\nid: 1\ndata:", "");
+            byte[] bytes = Convert.FromBase64String(data);
+            File.WriteAllBytes($"text2img/[masterpiece, best quality,{keyword}]_{seed + num}.jpg", bytes);
+        }
+
+        private void btn_Text2Img_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                TextToImage(seed);
+                Console.WriteLine(DateTime.Now + "生成完毕");
+            });
+        }
+
+        private void btn_Text2Imgs_Click(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                for (int i = 0; i < generateCount; i++)
+                {
+                    TextToImage(seed + i);
+                    Console.WriteLine(DateTime.Now + "生成完毕");
+                    await Task.Delay(5000);
+                }
+            });
+        }
+
+        private void btn_Config_Click(object sender, EventArgs e) => Process.Start("config.ini");
+
+        private void btn_images_Click(object sender, EventArgs e) => Process.Start("text2img");
+    }
+}
